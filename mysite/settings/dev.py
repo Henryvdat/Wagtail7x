cdabc51feb144b08674bd8403db06f4cc73b7e86
Henryvdat <neilw3d@gmail.com>
2026-05-16 14:33:33 -0400

    Initial commit: fresh Wagtail 7.4 project

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-buy^^f37tb$xp1$vzhsxh42xl5$5_b9-^h5xodvv-gg%2hayev"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
