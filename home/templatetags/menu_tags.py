2a463ae7c1d8005ae16dd6da5b8b48bc27c661e5
Neil Whittaker <neilw3d@gmail.com>
2026-05-21 21:00:23 -0400

    Center page titles on all pages; fix menu_tags W003 warning; add title to blog index

--- home/templatetags/menu_tags.py
+++
@@ -1,8 +0,0 @@
-# home/templatetags/menu_tags.py
-#
-# This file exists only to silence Django's W003 warning about duplicate
-# templatetag module names.  It re-exports the wagtailmenus register so that
-# {% load menu_tags %} in any template gets full access to wagtailmenus tags
-# (main_menu, flat_menu, etc.) even though `home` is listed before
-# `wagtailmenus` in INSTALLED_APPS.
-from wagtailmenus.templatetags.menu_tags import register  # noqa: F401
